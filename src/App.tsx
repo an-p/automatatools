0a208e73f75e7c2040c047b3ca8b6c90fd724f8e
Andreas Papon <andreas.papon@tum.de>
2021-07-30 15:48:07 +0200

    Add links to other tools.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import MinDFAComponent from './ui/algorithms/MinDFAComponent';
+import ToolLinks from './ui/ToolLinks';
 
 function App() {
   return (
-    <MinDFAComponent/>
+    <div>
+      <h1>Minimize DFA</h1>
+      <MinDFAComponent/>
+      <h1>Other useful tools</h1>
+      <ToolLinks/>
+      <hr/>
+      <a href="https://github.com/an-p/automatatools" rel="noreferrer" target="_blank">GitHub</a>
+    </div>
   );
 }
 
